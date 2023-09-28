c8887e8e8f9c9ea9c830026a67f2416590e5a94e
cavid32 <javidhal@code.edu.az>
2023-09-28 20:36:02 +0400

    kredit

---
+++ app.js
@@ -0,0 +1,30 @@
+const mebleg = document.getElementById("mebleg");
+const faiz = document.getElementById("faiz");
+const ayliq = document.getElementById("ayliq");
+const umumiMebleg = document.getElementById("umumiMebleg");
+const order = document.getElementById("order");
+const isdediyinmebleg = document.getElementById("isdediyinmebleg");
+
+let bankinfaizi = 0;
+let bankaodeyeceyim = 0;
+let ayliqodenis = 0;
+function kredit() {
+  bankinfaizi = (Number(mebleg.value) * Number(faiz.value)) / 100;
+  bankaodeyeceyim = Number(mebleg.value) + Number(bankinfaizi);
+  ayliqodenis = Number(bankaodeyeceyim) / Number(ayliq.value);
+  console.log(bankinfaizi.toFixed(2));
+  console.log(bankaodeyeceyim.toFixed(2));
+  console.log(ayliqodenis.toFixed(2));
+
+  umumiMebleg.textContent=ayliqodenis;
+}
+
+
+
+
+
+
+document.getElementById("mebleg").addEventListener("input", kredit);
+document.getElementById("umumiMebleg").addEventListener("umumiMebleg.textContent",kredit);
+document.getElementById("ayliq").addEventListener("input",kredit)
+document.getElementById("faiz").addEventListener("input",kredit);
