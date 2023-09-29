39a593916d01da8e934f3a250ae930d612686a7f
cavid32 <javidhal@code.edu.az>
2023-09-29 23:00:38 +0400

    kredit task commit

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ const ayliq = document.getElementById("ayliq");
 const umumiMebleg = document.getElementById("umumiMebleg");
 const order = document.getElementById("order");
 const isdediyinmebleg = document.getElementById("isdediyinmebleg");
+const LabelFaiz=document.getElementById("LabelFaiz");
+const LabelAy=document.getElementById("LabelAy");
+const muddet=document.getElementById("muddet"); 
+
+
 
 let bankinfaizi = 0;
 let bankaodeyeceyim = 0;
 let ayliqodenis = 0;
+
+
 function kredit() {
+  isdediyinmebleg.innerText=mebleg.value;
+  LabelFaiz.innerText=faiz.value;
+  LabelAy.innerText=ayliq.value
   bankinfaizi = (Number(mebleg.value) * Number(faiz.value)) / 100;
   bankaodeyeceyim = Number(mebleg.value) + Number(bankinfaizi);
   ayliqodenis = Number(bankaodeyeceyim) / Number(ayliq.value);
@@ -26,5 +36,5 @@ function kredit() {
 
 document.getElementById("mebleg").addEventListener("input", kredit);
 document.getElementById("umumiMebleg").addEventListener("umumiMebleg.textContent",kredit);
-document.getElementById("ayliq").addEventListener("input",kredit)
+document.getElementById("ayliq").addEventListener("input",kredit);
 document.getElementById("faiz").addEventListener("input",kredit);
